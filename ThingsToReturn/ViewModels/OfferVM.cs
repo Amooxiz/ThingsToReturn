1c6090a2de1925358e0f442dfc3d3bdd994df52b
slik margor <slikmargor@gmail.com>
2022-06-07 18:01:17 +0200

    Added retrieving offers

--- ThingsToReturn/ViewModels/OfferVM.cs
+++ ThingsToReturn/ViewModels/OfferVM.cs
@@ -2,6 +2,7 @@
 
 public class OfferVM
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string? ImagePath { get; set; }
     public string? Description { get; set; }
