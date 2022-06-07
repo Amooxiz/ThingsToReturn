3257b2fc7e42d86f5a52ca905277895e5f3887d6
slik margor <slikmargor@gmail.com>
2022-06-07 17:15:25 +0200

    Changed footer

--- ThingsToReturn/Services/OfferService.cs
+++ ThingsToReturn/Services/OfferService.cs
@@ -5,7 +5,7 @@ namespace ThingsToReturn.Services
     public class OfferService : IOfferService
     {
         private readonly IOfferRepository _offerRepository;
-
+           
         public OfferService(IOfferRepository offerRepository)
         {
             _offerRepository = offerRepository;
