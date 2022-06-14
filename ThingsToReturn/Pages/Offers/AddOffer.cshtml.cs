247cb4a8c761420d92ae5f0eba2145f3054ff2c8
slik margor <slikmargor@gmail.com>
2022-06-14 18:53:35 +0200

    <>

---
+++ ThingsToReturn/Pages/Offers/AddOffer.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThingsToReturn.Pages
+{
+    [BindProperties]
+    [Authorize]
+    public class AddOfferModel : PageModel
+    {
+        private readonly ILogger<AddOfferModel> _logger;
+        private readonly ICategoryService _categoryService;
+        private readonly IOfferCategoryService _offercategoryservice;
+        private readonly IOfferService _offerService;
+
+        public List<int> Categories { get; set; }
+        public Offer Offer { get; set; }
+        public CategoryToListVM CategoryList { get; set; }
+
+        public AddOfferModel(ILogger<AddOfferModel> logger, ICategoryService categoryService,
+            IOfferCategoryService offercategoryservice, IOfferService offerService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+            _offercategoryservice = offercategoryservice;
+            _offerService = offerService;
+        }
+        public void OnGet()
+        {
+            CategoryList = _categoryService.GetAllCategories();
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage();
+        }
+    }
+}
