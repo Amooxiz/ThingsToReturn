e24a901cf9b69ba031cdea6143d2d949c7c99c88
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-06-17 15:21:37 +0200

    Filtrate changes

--- ThingsToReturn/Interfaces/IAddressService.cs
+++ ThingsToReturn/Interfaces/IAddressService.cs
@@ -2,5 +2,6 @@
 {
     public interface IAddressService
     {
+        public List<string> GetAllCities();
     }
 }
--- ThingsToReturn/Interfaces/IOfferService.cs
+++ ThingsToReturn/Interfaces/IOfferService.cs
@@ -10,14 +10,7 @@ namespace ThingsToReturn.Interfaces
         public void CancelReservation(int offerId);
         public OfferToListVM Get20LatestOffers();
         public OfferToListVM GetUsersOffers(Claim claim);
-        public OfferToListVM FiltrateOffersByName(string offerName, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByCategoryId(int categoryId, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByNameAndCategoryId(string offerName, int categoryId, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByDate(DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByNameAndCity(string offerName, string city, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByCategoryIdAndCity(int categoryId, string city, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByCity(string city, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
-        public OfferToListVM FiltrateOffersByNameAndCategoryIdAndCity(string offerName, int categoryId, string city, DateTime createdDateDownLimit, DateTime createdDateUpLimit, DateTime expirationDateDownLimit, DateTime expirationDateUpLimit);
+        public OfferToListVM FiltrateOffers(FilterModel filterModel);
         public Offer GetOfferToDel(int offerId);
         public DateTime GetCreatedDateDownLimit();
         public DateTime GetCreatedDateUpLimit();
