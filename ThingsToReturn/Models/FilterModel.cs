081c26c4abc0d1d8a2814729a947f5defbb194f0
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-06-17 13:36:16 +0200

    added city and name to filterModel

--- ThingsToReturn/Models/FilterModel.cs
+++ ThingsToReturn/Models/FilterModel.cs
@@ -3,6 +3,8 @@
     public class FilterModel
     {
         public IList<int> Categories { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
         [DataType("Date")]
         public DateTime? CreationDateFrom { get; set; }
         [DataType("Date")]
