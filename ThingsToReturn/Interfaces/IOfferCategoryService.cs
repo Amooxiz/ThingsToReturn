c86f9a470d4ede4ac939946043a40d0258fbae54
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-06-19 11:16:56 +0200

    offerCategoryService changes

--- ThingsToReturn/Interfaces/IOfferCategoryService.cs
+++ ThingsToReturn/Interfaces/IOfferCategoryService.cs
@@ -1,9 +1,11 @@
-﻿namespace ThingsToReturn.Interfaces
+﻿using System.Security.Claims;
+
+namespace ThingsToReturn.Interfaces
 {
     public interface IOfferCategoryService
     {
         public CategoryToListVM GetCategoriesOfOffer(int offerId);
-        public void AddOffersWithCategories(Offer offer, IList<Category> categories);
+        public void AddOffersWithCategories(Offer Offer, IFormFile ImageFile, Address Address, Claim claim, IList<int> Categories);
 
     }
 }
--- ThingsToReturn/Services/OfferCategoryService.cs
+++ ThingsToReturn/Services/OfferCategoryService.cs
@@ -1,21 +1,49 @@
-﻿using ThingsToReturn.Extension;
+﻿using System.Security.Claims;
+using ThingsToReturn.Extension;
 
 namespace ThingsToReturn.Services
 {
     public class OfferCategoryService : IOfferCategoryService
     {
         private readonly IOfferCategoryRepository _offerCategoryRepository;
-        public OfferCategoryService(IOfferCategoryRepository offerCategoryRepository)
-            => _offerCategoryRepository = offerCategoryRepository;
-
-        public void AddOffersWithCategories(Offer offer, IList<Category> categories)
+        private readonly IAppUserService _appUserService;
+        private readonly ICategoryService _categoryService;
+        public OfferCategoryService(IOfferCategoryRepository offerCategoryRepository, IAppUserService appUserService, ICategoryService categoryService)
         {
+            _offerCategoryRepository = offerCategoryRepository;
+            _appUserService = appUserService;
+            _categoryService = categoryService;
+        }
+
+        public void AddOffersWithCategories(Offer Offer, IFormFile ImageFile, Address Address, Claim claim, IList<int> Categories)
+        {
+            var dir = Directory.GetCurrentDirectory();
+
+            //Tu rzeczy zwiazane ze zdj
+            var filename = ImageFile.FileName;
+            string filePathForDatabase = "/Images/" + filename;
+            string filePath = dir + "\\wwwroot" + "\\Images\\" + filename;
+
+            //Trzeba to pododawac adres do ofert i tam inne wymagan
+            Offer.ImagePath = filePathForDatabase;
+            Offer.CreatedDate = DateTime.Now;
+            Offer.Address = Address;
+            Offer.User = _appUserService.GetUser(claim.Value);
+
+            var categories = _categoryService.GetCategoriesByIdList(Categories);
+
+            //Zapis zdjeica
+            Stream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite);
+            ImageFile.CopyTo(fileStream);
+            fileStream.Dispose();
+
             var offercats = categories.Select(c => new OfferCategory
             {
 
                 Category = c,
-                Offer = offer,
+                Offer = Offer,
             }).ToList();
+
             _offerCategoryRepository.AddOffersWithCategories(offercats);
             
         }
